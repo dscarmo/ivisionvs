239724836605526a2d9726e53e1c25999de41af4
dscarmo <carmodiedre@outlook.com>
2015-03-03 16:47:38 -0300

    First Commit

---
+++ transformTest/transformTest/Transformer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace transformTest
+{
+    class Transformer
+    {
+        public void translatePoint(double inPoint, double offSet)
+        {
+
+        }
+    }
+}
