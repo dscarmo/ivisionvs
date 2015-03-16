541391de31ac9ddcc96040b874cbd3baec150712
dscarmo <carmodiedre@outlook.com>
2015-03-16 16:34:12 -0300

    Transform functions implemented

--- transformTest/transformTest/MainWindow.xaml.cs
+++ transformTest/transformTest/MainWindow.xaml.cs
@@ -27,26 +27,33 @@ namespace transformTest
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        {
-         
+        {         
                 translate();
-           
         }
 
         private void translate()
         {
-            double[] inJoint, translateVector;
+            double[] inJoint, translateVector, rotation, output, routput;
             Transformer transformer = new Transformer();
             try { 
                 inJoint = new double[3] { Double.Parse(x.Text), Double.Parse(y.Text), Double.Parse(z.Text) };
                 translateVector = new double[3] { Double.Parse(t1.Text), Double.Parse(t2.Text), Double.Parse(t3.Text) };
-                transformer.translatePoint(inJoint, ref translateVector);
-                xl.Text = translateVector[0].ToString();
-                yl.Text = translateVector[1].ToString();
-                zl.Text = translateVector[2].ToString();
+                rotation = new double[3] { Double.Parse(r1.Text), Double.Parse(r2.Text), Double.Parse(r3.Text) };
+                output = new double[3] { 0.0, 0.0, 0.0 };
+                routput = new double[3] { 0.0, 0.0, 0.0 };
+
+                transformer.transformPoint(inJoint, translateVector, rotation, ref output);
+                xl.Text = output[0].ToString("0.##");
+                yl.Text = output[1].ToString("0.##");
+                zl.Text = output[2].ToString("0.##");
+
+
+
             } catch (Exception) {
                 MessageBox.Show("Insira coordenadas nas entradas.");
             }
+
+            //pROCRUST DISTANCE
         }
 
         
--- transformTest/transformTest/Transformer.cs
+++ transformTest/transformTest/Transformer.cs
@@ -16,23 +16,70 @@ namespace transformTest
         /// <param name="inJoint"></param>
         /// <param name="offSet"></param>
         /// <returns></returns>
-        public void translatePoint(/*Joint inJoint*/ double[] inJoint, ref double[] offSet)
+        /// 
+       
+        ///Rotation
+        ///x' = x(cos(RZ)cos(RY)) + y(cos(RZ)sen(RY)sen(RX) - sen(RZ)cos(RX)) + z(cos(RZ)sen(RY)sen(RX) - sen(RZ)cos(RX)) + tx 
+        ///y' = x(sen(RZ)cos(RY)) + y(sen(RZ)sen(RY)sen(RX) + cos(RZ)cos(RX)) + z(sen(RZ)sen(RY)cos(RX) - cos(RZ)sen(RX)) + ty 
+        ///z' = x(-sen(RY)) + y(cos(RY)sen(RX)) + z(cos(RY)cos(RX)) + tz
+        public void transformPoint(/*Joint inJoint*/ double[] inJoint, double[] translateVector, double[] R, ref double[] output)
         {
-            /*For final version
-            double[] inVector = new double[3] { 0.0, 0.0, 0.0 };
-            inVector[0] = inJoint.Position.X;
-            inVector[1] = inJoint.Position.Y;
-            inVector[2] = inJoint.Position.Z;
-            */
+            //Inputs
+            double x, y, z, rx, ry, rz;
+            //Outputs
+            double X, Y, Z;
+   
 
-            //For Debug
-            for (int i = 0; i < 3; i++) {offSet[i] = offSet[i] + inJoint[i]; Console.WriteLine(offSet[i]);}
+            //Convert angle to radian
+            for (int i = 0; i < 3; i++) {
+                //output[i] = inJoint[i] + translateVector[i];
+                //routput[i] = Math.Sin(inJoint[i]);
+                //Convert rotations to radians
+                R[i] = deegresToRadians(R[i]);
+            }
+           
+            //Coordinates as in the formula
+            x = inJoint[0];
+            y = inJoint[1];
+            z = inJoint[2];
+            //Rotations as in the formula
+            rx = R[0];
+            ry = R[1];
+            rz = R[2];
+
+            //the final calculationnnnnn
+            
+            //X
+            X = x * (Cos(rz) * Cos(ry)) + y * (Cos(rz) * Sin(ry) * Sin(rx) - Sin(rz) * Cos(rx)) +
+                z * (Cos(rz) * Sin(ry) * Sin(rx) - Sin(rz) * Cos(rx)) + translateVector[0];
+
+            Y = x * (Sin(rz) * Cos(ry)) + y * (Sin(rz) * Sin(ry) * Sin(rx) + Cos(rz) * Cos(rx)) + 
+                z * (Sin(rz) * Sin(ry) * Cos(rx) - Cos(rz) * Sin(rx)) + translateVector[1];
+
+            Z = x * (-Sin(ry)) + y * (Cos(ry) * Sin(rx)) + z * (Cos(ry) * Cos(rx)) + translateVector[2];
+            //
+            output[0] = X;
+            output[1] = Y;
+            output[2] = Z;
+        }
+
+        private double deegresToRadians(double deegres)
+        {
+            return deegres * (Math.PI / 180);
+        }
+
+        private double Sin(double angle)
+        {
+            return Math.Sin(angle);
+        }
+
+        private double Cos(double angle)
+        {
+            return Math.Cos(angle);
+        }
+
+           
+            
         
-        }
-
-        public void rotatePoint(double[] offSet, ref double[] rotatedPoint)
-        {
-
-        }
     }
 }
