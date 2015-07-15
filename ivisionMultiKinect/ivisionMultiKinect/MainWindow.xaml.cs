aeb621db9ad477d66d636ab5d117d8c933cfe581
Yan <yanjovita@hotmail.com>
2015-07-15 15:54:56 -0300

    transferido a parte de ajuste de brilho para a classe Paint

--- ivisionMultiKinect/ivisionMultiKinect/MainWindow.xaml.cs
+++ ivisionMultiKinect/ivisionMultiKinect/MainWindow.xaml.cs
@@ -882,80 +882,15 @@ namespace multiKinect
             string dataset = "C:\\Users\\Public\\Kinect_Dataset\\Datatest";
             string filename = "";
             var FileNamePath = Path.Combine(dataset, filename);
-            string file = "";
-            while (i < x-1)
+            Paint brilho = new Paint();
+            int t = brilho.Brightness(dataset,x);
+            if (t != -1)
             {
-                for (int j = 0; j < 3; j++)
-                {
-                    if (j == 0)
-                    {
-                        //filename = "cam0_image" + i.ToString() + ".png";
-                        //FileNamePath = Path.Combine(dataset,filename);
-                        file = string.Concat(dataset, "\\cam0_image", i, ".png");
-                    }
-                    else
-                    {
-                        if (j == 1)
-                        {
-                            //filename = "cam1_image" + i.ToString() + ".png";
-                            //FileNamePath = Path.Combine(dataset, filename);
-                            file = string.Concat(dataset, "\\cam1_image", i, ".png");
-                        }
-                        else
-                        {
-                            if (j == 2)
-                            {
-                                //filename = "cam2_image" + i.ToString() + ".png";
-                                //FileNamePath = Path.Combine(dataset, filename);
-                                file = string.Concat(dataset, "\\cam2_image", i, ".png");
-                            }
-                            else
-                            {
-                                if (j == 3)
-                                {
-                                    //filename = "cam3_image" + i.ToString() + ".png";
-                                    //FileNamePath = Path.Combine(dataset, filename);
-                                    file = string.Concat(dataset, "\\cam3_image", i, ".png");
-                                }
-                            }
-                        }
-                    }
-                    /////////////////////
-                    Bitmap originalImage = null;
-                    using (var image = new Bitmap(file))
-                    {
-                        originalImage = new Bitmap(image);
-                    }
-                    Bitmap adjustedImage = new Bitmap(originalImage);
-                    float brightness = 1.0f;
-                    float contrast = 1.0f;
-                    float gamma = 1.2f;
-
-                    float adjustedBrightness = brightness - 1.0f;
-                    // create matrix that will brighten and contrast the image
-                    float[][] ptsArray ={
-                        new float[] {contrast, 0, 0, 0, 0}, // scale red
-                        new float[] {0, contrast, 0, 0, 0}, // scale green
-                        new float[] {0, 0, contrast, 0, 0}, // scale blue
-                        new float[] {0, 0, 0, 1.0f, 0}, // don't scale alpha
-                        new float[] {adjustedBrightness, adjustedBrightness, adjustedBrightness, 0, 1}};
-
-                    ImageAttributes imageAttributes = new ImageAttributes();
-                    imageAttributes.ClearColorMatrix();
-                    imageAttributes.SetColorMatrix(new ColorMatrix(ptsArray), ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
-                    imageAttributes.SetGamma(gamma, ColorAdjustType.Bitmap);
-
-                    Graphics g = Graphics.FromImage(adjustedImage);
-                    g.DrawImage(originalImage, new Rectangle(0, 0, adjustedImage.Width, adjustedImage.Height)
-                        , 0, 0, originalImage.Width, originalImage.Height,
-                        GraphicsUnit.Pixel, imageAttributes);
-
-                    adjustedImage.Save(file);
-                    
-                    /////////////////////
-                    i++;
-                }
-                
+                MessageBox.Show("Brilho alterado em "+t.ToString()+" imagens.");
+            }
+            else
+            {
+                MessageBox.Show("Erro na alteração do brilho. "+t.ToString()+" imagens alteradas.");
             }
         }
         #endregion
--- ivisionMultiKinect/ivisionMultiKinect/Paint.cs
+++ ivisionMultiKinect/ivisionMultiKinect/Paint.cs
@@ -4,11 +4,109 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace ivisionMultiKinect
 {
     class Paint
     {
-       
+       public int Brightness(string dataset, int x)
+        {
+            string file = "";
+            int cont = 0;
+            try
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (j == 0)
+                        {
+                            file = string.Concat(dataset, "\\cam0_image", i, ".png");
+                        }
+                        else
+                        {
+                            if (j == 1)
+                            {
+                                file = string.Concat(dataset, "\\cam1_image", i, ".png");
+                            }
+                            else
+                            {
+                                if (j == 2)
+                                {
+                                    file = string.Concat(dataset, "\\cam2_image", i, ".png");
+                                }
+                                else
+                                {
+                                    if (j == 3)
+                                    {
+                                        file = string.Concat(dataset, "\\cam3_image", i, ".png");
+                                    }
+                                }
+                            }
+                        }
+                        
+                        Bitmap originalImage = null;
+                        using (var image = new Bitmap(file))
+                        {
+                            originalImage = new Bitmap(image);
+                        }
+                        Bitmap adjustedImage = new Bitmap(originalImage);
+                        float brightness = 1.0f;
+                        float contrast = 1.0f;
+                        float gamma = 1.0f;
+
+                        float adjustedBrightness = brightness - 1.0f;
+                        // create matrix that will brighten and contrast the image
+                        float[][] ptsArray ={
+                        new float[] {contrast, 0, 0, 0, 0}, // scale red
+                        new float[] {0, contrast, 0, 0, 0}, // scale green
+                        new float[] {0, 0, contrast, 0, 0}, // scale blue
+                        new float[] {0, 0, 0, 1.0f, 0}, // don't scale alpha
+                        new float[] {adjustedBrightness, adjustedBrightness, adjustedBrightness, 0, 1}};
+
+                        ImageAttributes imageAttributes = new ImageAttributes();
+                        imageAttributes.ClearColorMatrix();
+                        imageAttributes.SetColorMatrix(new ColorMatrix(ptsArray), ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+                        imageAttributes.SetGamma(gamma, ColorAdjustType.Bitmap);
+
+                        if (j == 0)
+                        {
+                            file = string.Concat(dataset, "\\brilho0_image", i, ".png");
+                        }
+                        else
+                        {
+                            if (j == 1)
+                            {
+                                file = string.Concat(dataset, "\\brilho1_image", i, ".png");
+                            }
+                            else
+                            {
+                                if (j == 2)
+                                {
+                                    file = string.Concat(dataset, "\\brilho2_image", i, ".png");
+                                }
+                                else
+                                {
+                                    if (j == 3)
+                                    {
+                                        file = string.Concat(dataset, "\\brilho3_image", i, ".png");
+                                    }
+                                }
+                            }
+                        }
+
+                        adjustedImage.Save(file);
+                        cont++;
+                    }
+
+                }
+                return cont;
+            }
+            catch 
+            {
+                return -1;
+            }
+        }
     }
 }
